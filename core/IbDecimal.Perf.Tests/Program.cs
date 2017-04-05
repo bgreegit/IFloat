f63e604667e15b24d225cb7dffa39b618f2c41d9
bgree <bgree@mogloogames.com>
2017-04-05 10:47:02 +0900

    add perf test for + * /

--- core/IbDecimal.Perf.Tests/Program.cs
+++ core/IbDecimal.Perf.Tests/Program.cs
@@ -7,9 +7,18 @@ namespace IbReal.Perf.Tests
     {
         static void Main(string[] args)
         {
-            TestDouble();
-            TestFloat();
-        }
+			Console.WriteLine("<TestDouble>");
+			TestDouble();
+			Console.WriteLine(Environment.NewLine);
+
+			Console.WriteLine("<TestFloat>");
+			TestFloat();
+			Console.WriteLine(Environment.NewLine);
+
+			Console.WriteLine("<TestDouble>");
+			TestDecimal();
+			Console.WriteLine(Environment.NewLine);
+		}
 
         static void TestFloat()
         {
@@ -190,6 +199,97 @@ namespace IbReal.Perf.Tests
             }
             sw.Stop();
             Console.WriteLine($"IbDouble / : {sw.Elapsed}");
+            sw.Reset();
+        }
+
+		static void TestDecimal()
+        {
+            Stopwatch sw = new Stopwatch();
+
+            int loopCount = 10000000;
+            decimal rd0, rd1, rd;
+            rd0 = 123.456m;
+            rd1 = 654.321m;
+            IbFloat id0, id1, id;
+            id0 = new IbFloat(123456, -3);
+            id1 = new IbFloat(654321, -3);
+
+            // +
+
+            sw.Start();
+            {
+                rd = 0;
+                for (int i = 0; i < loopCount; ++i)
+                {
+                    rd += rd0 + rd1;
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"decimal + : {sw.Elapsed}");
+            sw.Reset();
+
+            sw.Start();
+            {
+                id = IbFloat.Zero;
+                for (int i = 0; i < loopCount; ++i)
+                {
+                    id += id0 + id1;
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"IbFloat + : {sw.Elapsed}");
+            sw.Reset();
+
+            // *
+
+            sw.Start();
+            {
+                rd = 0;
+                for (int i = 0; i < loopCount; ++i)
+                {
+                    rd += rd0 * rd1;
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"decimal * : {sw.Elapsed}");
+            sw.Reset();
+
+            sw.Start();
+            {
+                id = IbFloat.Zero;
+                for (int i = 0; i < loopCount; ++i)
+                {
+                    id += id0 * id1;
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"IbFloat * : {sw.Elapsed}");
+            sw.Reset();
+
+            // /
+
+            sw.Start();
+            {
+                rd = 0;
+                for (int i = 0; i < loopCount; ++i)
+                {
+                    rd += rd0 / rd1;
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"decimal / : {sw.Elapsed}");
+            sw.Reset();
+
+            sw.Start();
+            {
+                id = IbFloat.Zero;
+                for (int i = 0; i < loopCount; ++i)
+                {
+                    id += id0 / id1;
+                }
+            }
+            sw.Stop();
+            Console.WriteLine($"IbFloat / : {sw.Elapsed}");
             sw.Reset();
         }
     }
